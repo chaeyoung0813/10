24f211a22916e03d1c63b321035e966f846701da
chaeyoung0813 <chaeyoung0813@naver.com>
2022-11-08 16:53:07 +0900

    4

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char src[]="The worst things to eat before you sleep";
-	char dst[100];
-	strcpy(dst, src);
-	printf("copled string : %s", dst);
+	char str[30]="Happy C programming";
+	printf("문자열의 %s의 길이 : %i\n", str, strlen(str));
+	return 0;
+	
 }
